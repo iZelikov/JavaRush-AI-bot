b0e50fba469b526a2ec7ac419d3431504aef7589
iZelikov <ivan@zelikov.name>
2025-08-05 17:41:28 +0300

    refactor: include routers in handlers package __init__.py

---
+++ app/handlers/__init__.py
@@ -0,0 +1,18 @@
+from aiogram import Router
+
+from config import ENV
+from handlers.cmd_handlers import cmd_router
+from handlers.dialogs_handlers import dialog_router
+from handlers.msg_handlers import msg_router
+from handlers.test_handlers import test_router
+
+main_router = Router()
+
+if ENV == 'dev':
+    main_router.include_routers(test_router)
+
+main_router.include_routers(cmd_router, dialog_router, msg_router)
+
+__all__ = [
+    'main_router',
+]
